b14f38c7b8a3bc82a2c1c80f5aed5937e3e2328c
Gucci Fan <gucc@umich.edu>
2022-08-07 15:46:27 -0400

    made explore all movies functional, updated README with questions

--- README.md
+++ README.md
@@ -3,31 +3,50 @@
 ## GHIBLI MOVIE LIST MAKER
 
 Top text:
-Curious about Studio Ghibli movies but don't know where to start? Or have you seen a few and want a suggestion of what to watch next?
+Make a list of your favorite Studio Ghibli movies and keep track of the ones you want to watch next!
+
+If you've never seen any Studio Ghibli movies and don't know where to start, hit Explore All Movies.
+
+https://ghibliapi.herokuapp.com/films
 
 Basic idea:
-- Creates 2 lists: TO WATCH and WATCHED
-- Returns a randomly generated movie from a search bar based on title or randomly generated movie
+- Creates 2 lists of ghibli movies: TO WATCH and WATCHED
+- Returns a movie from a search bar based on title, or explore all movies
 - "Add to my list" or "search again"/"next result"
 
 Basic layout:
 - Top: search
-    - 1. Search bar + space for randomly generated movie to appear
-        - Text: "search by title or nothing if you want a surprise."
-        - Buttons: search, want to watch, already watched 
+    - 1. Explore All Movies button
+        - returns all movies images, mousing over lets you see the title/year
+        - clicking gives you detailed movie page like how search would return
+    - 2. Search bar + space for result to appear (prefer one at a time?)
+        - Text: "search for movie by title"
+        - Buttons: search, add to watch list, add to favorites list
         - Image: movie image, title, original title, year, description
 - Bottom: two columns
     - 1. TO WATCH list
         - assembles movies from the first column that user hits "add to my list" on
         - un-numbered list
         - stores movie poster + title, year
-        - button: delete, clear list, "watched" moves movie from TO WATCH to WATCHED
-    - 2. WATCHED list
+        - button: delete, clear list, "favorite" moves movie from TO WATCH to FAVORITE
+    - 2. FAVORITE list
+        - unnumbered list
         - assembles movies from TO WATCH list or "already watched" button
-        - heart for favorite?
+        - delete button / clear list
 
 
-Things I'm unsure about:
+I have questions about:
+
+8/06 (resolved)
 - How to connect an API
 - How to find out what data my API has and how to use it
 - How to return search results? How to randomly generate result?
+
+8/07
+Formatting the explore all movies thing:
+- how to do a mouseover event on an image that makes a little overlay with the title and year
+Formattimg search results from search bar:
+- splitting search results into pages
+- adding button for favorite, watch later
+- if nothing is put in the search bar, how to stop returning all results when submit is clicked
+Making to watch / watched list
--- script.js
+++ script.js
@@ -2,23 +2,55 @@ const h2 = document.createElement("h2");
 h2.textContent = "";
 document.querySelector("body").appendChild(h2);
 
+const searchResult = document.getElementById("search-result")
+const exploreResult = document.getElementById("explore-result")
+
+
+const allMovies = document.getElementById("all-movies")
+allMovies.addEventListener("click", (event) => {
+    event.preventDefault()
+    searchResult.innerHTML = ""
+    exploreResult.innerHTML = ""
+    fetch(`https://ghibliapi.herokuapp.com/films`)
+    .then(response => response.json())
+    .then(response => {
+        const p = document.createElement("p")
+        response.map(item => {
+            const img = document.createElement("img")
+            img.src = item.image
+            const titleAndYear = document.createElement("h2")
+            titleAndYear.innerText = item.title + " (" + item.release_date + ")"
+            p.append(img)
+            exploreResult.append(p)
+            //img.addEventListener("mouseover", () => console.log(titleAndYear))
+            //add a mouseover event that shows the title and year of the movie overlaying the image
+            //and then disappears when you mouse off
+            img.addEventListener("click", (event) => {
+                event.preventDefault()
+                exploreResult.innerHTML = ""
+                exploreResult.append(img, titleAndYear)
+            })
+        })
+    })
+})
+
+
 const form = document.getElementById("movie-search")
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-    //console.log("event", event)
+    searchResult.innerHTML = ""
+    exploreResult.innerHTML = ""
     // data we want to pass from the form: what you searched
     //event.target[0].value
     fetch(`https://ghibliapi.herokuapp.com/films?q=${event.target[0].value}`)
     .then(response => response.json())
     .then(response => {
-        console.log(response[0].title)
-        const searchResult = document.getElementById("search-result")
-        searchResult.innerHTML = ""
+        //console.log(response[0].title)
         const li = document.createElement("ul")
         response.map(item =>{
             const img = document.createElement("img")
             img.src = item.image
-            const titleAndYear = document.createElement("h3")
+            const titleAndYear = document.createElement("h2")
             titleAndYear.innerText = item.title + " (" + item.release_date + ")"
             const ogTitle = document.createElement("h4")
             ogTitle.innerText = item.original_title
@@ -30,6 +62,7 @@ form.addEventListener("submit", (event) => {
             searchResult.append(li)
         })
     })
+    
     form.reset()
 })
     // I want to iterate through the array of objects to find the object
@@ -54,7 +87,7 @@ form.addEventListener("submit", (event) => {
             userList.append(li)
         })*/
 
-const randomizeButton = document.getElementById("randomize")
+/*const randomizeButton = document.getElementById("randomize")
 randomizeButton.addEventListener("click", (event) =>{
     event
-})
+})*/
