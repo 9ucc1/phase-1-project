fbf9e1919b097da4c16b3f0eb679d57ffc026e78
Gucci Fan <gucc@umich.edu>
2022-10-01 22:10:40 -0400

    finished refactoring descrip page and watch list functions, deleted excess code

--- script.js
+++ script.js
@@ -1,41 +1,77 @@
-const searchResult = document.getElementById("search-result")
-const exploreResult = document.getElementById("explore-result")
-const appTitle = document.getElementById("big-title")
 const homescreen = document.getElementById("homescreen")
-const watchListButton = document.getElementById("watch")
-const watchButton = document.getElementById("add-to-watch")
-const clearListButton = document.getElementById("clear-list")
-    clearListButton.style.visibility = "hidden"
-const watchList = document.getElementById("watch-list")
-const watchListEntries = document.getElementById("watch-list-entries")
-    watchList.style.visibility = "hidden"
-watchListButton.addEventListener("click", () => {
-    exploreResult.innerHTML = ""
-    searchResult.innerHTML = ""
-    homescreen.style.display = "none"
-    watchList.style.visibility = "visible"
-    clearListButton.style.visibility = "visible"
-})
-
+const result = document.getElementById("result")
+const appTitle = document.getElementById("big-title")
 appTitle.addEventListener("click", (event) => {
     homescreen.style.display = "block"
-    watchList.style.visibility = "hidden"
+    watchListContainer.style.visibility = "hidden"
     clearListButton.style.visibility = "hidden"
-    exploreResult.innerHTML = ""
-    searchResult.innerHTML = ""
+    result.innerHTML = ""
+})
+const watchListContainer = document.getElementById("watch-list")
+    watchListContainer.style.visibility = "hidden"
+const watchListButton = document.getElementById("watch")
+    watchListButton.addEventListener("click", () => {
+        result.innerHTML = ""
+        homescreen.style.display = "none"
+        watchListContainer.style.visibility = "visible"
+        clearListButton.style.visibility = "visible"
+    })
+const watchListEntries = document.getElementById("watch-list-entries")
+const clearListButton = document.getElementById("clear-list")
+    clearListButton.style.visibility = "hidden"
+    clearListButton.addEventListener("click", ()=> {
+        watchListEntries.innerHTML = ""
+        alert("List cleared!")
+    })
+
+const allMovies = document.getElementById("all-movies")
+allMovies.addEventListener("click", (event) => {
+    result.innerHTML = ""
+    watchListContainer.style.visibility = "hidden"
+    clearListButton.style.visibility = "hidden"
+    homescreen.style.display = "none"
+    fetch(`https://ghibliapi.herokuapp.com/films`)
+    .then(response => response.json())
+    .then(response => {
+        const p = document.createElement("p")
+        response.map(item => {
+            const imgList = document.createElement("img")
+            imgList.src = item.image
+            p.append(imgList)
+            result.append(p)
+            imgList.setAttribute("id", "allmovieslist")
+            imgList.addEventListener("mouseover", (event) => {
+                imgList.style.border = "solid"
+            })
+            imgList.addEventListener("mouseleave", (event) => {
+                imgList.style.border= "none"
+            })
+            imgList.addEventListener("click", (event) => {
+                result.innerHTML = ""
+                descriptionPage(item)
+            })
+        })
+    })
 })
 
-clearListButton.addEventListener("click", ()=> {
-    watchListEntries.innerHTML = ""
-    alert("List cleared!")
+const form = document.getElementById("movie-search")
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    result.innerHTML = ""
+    watchListContainer.style.visibility = "hidden"
+    clearListButton.style.visibility = "hidden"
+    homescreen.style.display = "none"
+    fetch(`https://ghibliapi.herokuapp.com/films?q=${event.target[0].value}`)
+    .then(response => response.json())
+    .then(response => {
+        response.map(item =>{
+            descriptionPage(item)
+        })
+    })
+    form.reset()
 })
-
-//start of new stuff//
-
-let fetchResponse
 
 function descriptionPage(item){
-    exploreResult.innerHTML= ""
     const { image, title, release_date, original_title, director, description, movie_banner } = item
     const img = document.createElement("img")
         img.src = image
@@ -48,191 +84,36 @@ function descriptionPage(item){
         directedBy.innerText = "Directed by " + director
     const descriptionText = document.createElement("p")
         descriptionText.innerText = description
+        descriptionText.setAttribute("id", "description")
     const watchButton = document.createElement("button")
         watchButton.setAttribute("id", "add-to-watch")
         watchButton.innerText = "Add to Watch List"
-        const watchListEntry = document.createElement("li")
-                watchListEntry.innerText = title + " (" + release_date + ")"
-                watchListEntry.setAttribute("title", "remove from watch list")    
-                watchListEntry.addEventListener("mouseover", (event) => {
-                        watchListEntry.style.textDecoration = "line-through"
-                        watchListEntry.style.cursor = "pointer"
-                    })
-                    watchListEntry.addEventListener("mouseleave", (event) => {
-                        watchListEntry.style.textDecoration = "none"
-                    })
-                    watchListEntry.addEventListener("click", (event) => {
-                        watchListEntry.remove()
-                    })
-                watchButton.addEventListener("click", (event) => {
-                    watchListEntries.append(watchListEntry)
-                    alert("Added to watch list!")
-                })
+        watchButton.addEventListener("click", (event) => {
+            watchListEntries.append(watchListFunction(item))
+                alert("Added to watch list!")
+            })
     const lineBreak = document.createElement("br")
+    const lineBreak2 = document.createElement("br")
     const banner = document.createElement("img")
         banner.src = movie_banner
         banner.setAttribute("id", "banner")
-    exploreResult.append(img, titleAndYear, ogTitle, directedBy, description, lineBreak, watchButton, lineBreak, banner)
+    result.append(img, titleAndYear, ogTitle, directedBy, description, lineBreak, watchButton, lineBreak2, banner)
 }
 
-//function descriptionPages(descriptionPage){}
-
-function watchButtonListeners(){
-    console.log("hi")
+function watchListFunction(item){
+    const { title, release_date } = item
     const watchListEntry = document.createElement("li")
-                //watchListEntry.innerText = title + " (" + release_date + ")"
-                watchListEntry.innerText = "hi"
-                watchListEntry.setAttribute("title", "remove from watch list")    
-                watchListEntry.addEventListener("mouseover", (event) => {
-                        watchListEntry.style.textDecoration = "line-through"
-                        watchListEntry.style.cursor = "pointer"
-                    })
-                    watchListEntry.addEventListener("mouseleave", (event) => {
-                        watchListEntry.style.textDecoration = "none"
-                    })
-                    watchListEntry.addEventListener("click", (event) => {
-                        watchListEntry.remove()
-                    })
-                
-                watchButton.addEventListener("click", (event) => {
-                    console.log("clicked")
-                    watchListEntries.append(watchListEntry)
+        watchListEntry.innerText = title + " (" + release_date + ")"
+        watchListEntry.setAttribute("title", "remove from watch list")    
+        watchListEntry.addEventListener("mouseover", (event) => {
+            watchListEntry.style.textDecoration = "line-through"
+            watchListEntry.style.cursor = "pointer"
                 })
-                watchButton.addEventListener("click", event => alert("Added to watch list!"))
+        watchListEntry.addEventListener("mouseleave", (event) => {
+            watchListEntry.style.textDecoration = "none"
+                })
+        watchListEntry.addEventListener("click", (event) => {
+            watchListEntry.remove()
+                })
+    return watchListEntry
 }
-
-//end of new stuff
-
-const allMovies = document.getElementById("all-movies")
-allMovies.addEventListener("click", (event) => {
-    searchResult.innerHTML = ""
-    exploreResult.innerHTML = ""
-    watchList.style.visibility = "hidden"
-    clearListButton.style.visibility = "hidden"
-    homescreen.style.display = "none"
-    fetch(`https://ghibliapi.herokuapp.com/films`)
-    .then(response => response.json())
-    .then(response => {
-        fetchResponse = response
-
-        const p = document.createElement("p")
-        response.map(item => {
-            
-            const imgList = document.createElement("img")
-            imgList.src = item.image
-            p.append(imgList)
-            exploreResult.append(p)
-            imgList.setAttribute("id", "allmovieslist")
-            imgList.addEventListener("click", (event) => {
-                descriptionPage(item)
-                /*exploreResult.innerHTML = ""
-                const { image } = item
-                const img = document.createElement("img")
-                img.src = image
-                img.setAttribute("id", "explore-img")
-                const titleAndYear = document.createElement("h2")
-                titleAndYear.innerText = item.title + " (" + item.release_date + ")"
-                const ogTitle = document.createElement("h4")
-                ogTitle.innerText = item.original_title
-                const director = document.createElement("h4")
-                director.innerText = "Directed by " + item.director
-                const description = document.createElement("p")
-                description.innerText = item.description
-                description.setAttribute("id", "description")
-                const watchButton = document.createElement("button")
-                watchButton.setAttribute("id", "add-to-watch")
-                watchButton.innerText = "Add to Watch List"
-                watchButtonListeners()
-                const lineBreak = document.createElement("br")
-                const banner = document.createElement("img")
-                    banner.src = item.movie_banner
-                    banner.setAttribute("id", "banner")
-                exploreResult.append(img, titleAndYear, ogTitle, director, description, watchButton, lineBreak, banner)
-                const watchListEntry = document.createElement("li")
-                watchListEntry.innerText = item.title + " (" + item.release_date + ")"
-                watchListEntry.setAttribute("title", "remove from watch list")    
-                watchListEntry.addEventListener("mouseover", (event) => {
-                        watchListEntry.style.textDecoration = "line-through"
-                        watchListEntry.style.cursor = "pointer"
-                    })
-                    watchListEntry.addEventListener("mouseleave", (event) => {
-                        watchListEntry.style.textDecoration = "none"
-                    })
-                    watchListEntry.addEventListener("click", (event) => {
-                        watchListEntry.remove()
-                    })
-                watchButton.addEventListener("click", (event) => {
-                    watchListEntries.append(watchListEntry)
-                })
-                watchButton.addEventListener("click", event => alert("Added to watch list!"))*/
-            })
-            imgList.addEventListener("mouseover", (event) => {
-                imgList.style.border = "solid"
-            })
-            imgList.addEventListener("mouseleave", (event) => {
-                imgList.style.border= "none"
-            })
-        })
-    })
-})
-
-const form = document.getElementById("movie-search")
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    searchResult.innerHTML = ""
-    exploreResult.innerHTML = ""
-    watchList.style.visibility = "hidden"
-    clearListButton.style.visibility = "hidden"
-    homescreen.style.display = "none"
-    fetch(`https://ghibliapi.herokuapp.com/films?q=${event.target[0].value}`)
-    .then(response => response.json())
-    .then(response => {
-        const li = document.createElement("ul")
-        response.map(item =>{
-            const img = document.createElement("img")
-            img.src = item.image
-            img.setAttribute("id", "search-img")
-            const watchButton = document.createElement("button")
-            watchButton.setAttribute("id", "add-to-watch")
-            watchButton.innerText = "Add to Watch List"
-            const titleAndYear = document.createElement("h2")
-            titleAndYear.innerText = item.title + " (" + item.release_date + ")"
-            const ogTitle = document.createElement("h4")
-            ogTitle.innerText = item.original_title
-            const director = document.createElement("h4")
-            director.innerText = "Directed by " + item.director
-            const description = document.createElement("p")
-            description.innerText = item.description
-            description.setAttribute("id", "description")
-            const banner = document.createElement("img")
-                banner.src = item.movie_banner
-            banner.setAttribute("id", "banner")
-            const lineBreak = document.createElement("br")
-            li.append(img, titleAndYear, ogTitle, director, description, watchButton, lineBreak, banner)
-            searchResult.append(li)
-            const watchListEntry = document.createElement("li")
-                watchListEntry.innerText = item.title + " (" + item.release_date + ")"
-                watchListEntry.setAttribute("title", "remove from watch list") 
-                watchListEntry.addEventListener("mouseover", (event) => {
-                    watchListEntry.style.textDecoration = "line-through"
-                    watchListEntry.style.cursor = "pointer"
-                })
-                watchListEntry.addEventListener("mouseleave", (event) => {
-                    watchListEntry.style.textDecoration = "none"
-                })
-                watchListEntry.addEventListener("click", (event) => {
-                    watchListEntry.remove()
-                })
-                watchButton.addEventListener("click", (event) => {
-                    watchListEntries.append(watchListEntry)
-                })
-                watchButton.addEventListener("click", event => alert("Added to watch list!"))
-        })
-    })
-    form.reset()
-})
-
-/*fetchResponse.forEach(function(movie){
-    console.log(`${movie}`)
-}
-    (item) => descriptionPage(item))*/
